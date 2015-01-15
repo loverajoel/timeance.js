d764153ab97b504e526407a98ef732d0c2fc4feb
homer0 <leo.apiwan@olapic.com>
2015-01-15 18:14:17 -0300

    Added AMD support and DS_Store on the gitignore

--- src/timeance.js
+++ src/timeance.js
@@ -4,61 +4,72 @@
  * MIT licensed
  */
 
+;(function(root, factory){
+    if(typeof define === 'function' && define.amd){
+        define([],factory);
+    }else{
+        root.Timeance = factory;
+    }
+})(this, (function(){
+
 var Timeance = (function(window) {
-    'use strict';
+        'use strict';
 
-    var performance = (typeof window.performance !== 'undefined') ? window.performance : undefined,
-        customEvents = [];
+        var performance = (typeof window.performance !== 'undefined') ? window.performance : undefined,
+            customEvents = [];
 
-    /*
-     * Function that return the mapped data, and append to
-     * it, default info like, resources and timing.
-     */
-    var _getResponse = function() {
-        return {
-            resources: performance.getEntries(),
-            timing: performance.timing,
-            customEvents: customEvents
-        };
-    };
-
-    /*
-     * Function that allow inyect custom events.
-     * Push the event to the _customerEvents var.
-     * @param info {string|object}
-     */
-    var _event = function(info) {
-        var data = {
-            event: info,
-            time: performance.now()
-        };
-        customEvents.push(data);
-    };
-
-    /*
-     * Function that finish the record info.
-     * @param callback {function}
-     * @param wait {bool} *optional if is true, the response
-     * will wait for the window.onload event fire
-     */
-    var _endResponse = function(callback, wait) {
-        if (wait) {
-            window.onload = function() {
-                callback(_getResponse());
+        /*
+         * Function that return the mapped data, and append to
+         * it, default info like, resources and timing.
+         */
+        var _getResponse = function() {
+            return {
+                resources: performance.getEntries(),
+                timing: performance.timing,
+                customEvents: customEvents
             };
-        } else {
-            callback(_getResponse());
-        }
-    };
+        };
 
-    /*
-     * Public events
-     */
-    var methods = {
-        event: performance ? _event : function(){},
-        end: performance ? _endResponse : function(){}
-    };
+        /*
+         * Function that allow inyect custom events.
+         * Push the event to the _customerEvents var.
+         * @param info {string|object}
+         */
+        var _event = function(info) {
+            var data = {
+                event: info,
+                time: performance.now()
+            };
+            customEvents.push(data);
+        };
 
-    return methods;
+        /*
+         * Function that finish the record info.
+         * @param callback {function}
+         * @param wait {bool} *optional if is true, the response
+         * will wait for the window.onload event fire
+         */
+        var _endResponse = function(callback, wait) {
+            if (wait) {
+                window.onload = function() {
+                    callback(_getResponse());
+                };
+            } else {
+                callback(_getResponse());
+            }
+        };
 
-})(this);
+        /*
+         * Public events
+         */
+        var methods = {
+            event: performance ? _event : function(){},
+            end: performance ? _endResponse : function(){}
+        };
+
+        return methods;
+
+    })(this);
+    return Timeance;
+
+}));
