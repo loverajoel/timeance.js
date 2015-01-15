efcb2a6ddc7c2a8783704792f31fb56715e62e99
Joel <joelalovera@gmail.com>
2015-01-15 00:05:34 -0300

    add gulpfile and dependeces

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp    = require("gulp"),
+    plugins = require("gulp-load-plugins")();
+
+gulp.task("scripts", function () {
+    gulp
+        .src("src/timeance.js")
+        .pipe(gulp.dest("dist"))
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["scripts"]);
